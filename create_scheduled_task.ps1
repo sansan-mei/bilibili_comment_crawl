afbcf6d6eeed068b4459030cff415c2caa969001
梅花三十三 <1596944197@qq.com>
2025-05-02 19:28:17 +0800

    chore: 取消开机自启动脚本

--- create_scheduled_task.ps1
+++
@@ -1,5 +0,0 @@
-$Action = New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/c D:\code\python\bilibili_comment_crawl\start_crawler.bat"
-$Trigger = New-ScheduledTaskTrigger -AtStartup
-$Settings = New-ScheduledTaskSettingsSet -StartWhenAvailable -DontStopOnIdleEnd
-$Principal = New-ScheduledTaskPrincipal -UserId "$env:COMPUTERNAME\$env:USERNAME" -LogonType S4U
-Register-ScheduledTask -TaskName "BilibiliCommentCrawler" -Action $Action -Trigger $Trigger -Settings $Settings -Principal $Principal -Description "开机自动运行B站评论爬虫" 
